45522ac4e4e248efba434084d36eacf8f13d58d3
aycabta <aycabta@gmail.com>
2020-03-09 19:38:51 +0900

    Add vterm gem existence check logic

--- lib/yamatanooroti.rb
+++ lib/yamatanooroti.rb
@@ -1,4 +1,14 @@
 class Yamatanooroti
+  def Yamatanooroti.const_missing(id)
+    case id
+    when :VTermTestCase
+      begin
+        require 'vterm'
+        require 'yamatanooroti/vterm'
+        Yamatanooroti::VTermTestCase
+      rescue LoadError
+        raise 'You need vterm gem for Yamatanooroti::VTermTestCase'
+      end
+    end
+  end
 end
-
-require 'yamatanooroti/vterm'
