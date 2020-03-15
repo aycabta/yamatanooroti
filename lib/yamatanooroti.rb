7eebbaec2c097e5a7a17473e6894a9bc751c8593
aycabta <aycabta@gmail.com>
2020-03-15 20:52:03 +0900

    Arrange dynamic loading of test case classes

--- lib/yamatanooroti.rb
+++ lib/yamatanooroti.rb
@@ -1,20 +1,32 @@
 class Yamatanooroti
-  def Yamatanooroti.const_missing(id)
+  def self.load_vterm
+    require 'vterm'
+    require 'yamatanooroti/vterm'
+  rescue LoadError
+    raise LoadError.new('You need vterm gem for Yamatanooroti::VTermTestCase')
+  end
+
+  def self.load_windows
+    unless win?
+      raise LoadError.new('You need Windows environment for Yamatanooroti::WindowsTestCase')
+    end
+    require 'yamatanooroti/windows'
+  end
+
+  def self.const_missing(id)
     case id
     when :VTermTestCase
-      begin
-        require 'vterm'
-        require 'yamatanooroti/vterm'
-        Yamatanooroti::VTermTestCase
-      rescue LoadError
-        raise LoadError.new('You need vterm gem for Yamatanooroti::VTermTestCase')
-      end
+      load_vterm
+      Yamatanooroti::VTermTestCase
     when :WindowsTestCase
-      unless RbConfig::CONFIG['host_os'].match?(/mswin|msys|mingw|cygwin|bccwin|wince|emc/)
-        raise LoadError.new('You need Windows environment for Yamatanooroti::WindowsTestCase')
-      end
-      require 'yamatanooroti/windows'
+      load_windows
       Yamatanooroti::WindowsTestCase
+    else
+      raise StandardError.new("Unknown class #{id.to_s}")
     end
+  end
+
+  def self.win?
+    RbConfig::CONFIG['host_os'].match?(/mswin|msys|mingw|cygwin|bccwin|wince|emc/)
   end
 end
