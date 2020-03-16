fa6e172e226ae7e89174bf7b347f9909e22f2ecf
aycabta <aycabta@gmail.com>
2020-03-16 21:30:44 +0900

    Remove unnecessary method in a test suite

--- test/yamatanooroti/test_vterm.rb
+++ test/yamatanooroti/test_vterm.rb
@@ -1,9 +1,6 @@
 require 'yamatanooroti'
 
 class Yamatanooroti::TestVTerm < Test::Unit::TestCase
-  def setup
-  end
-
   def test_load
     if Yamatanooroti.has_vterm_gem?
       assert_nothing_raised do
