ed22e77a0319a1bc45548b9f497635933666ac79
aycabta <aycabta@gmail.com>
2020-06-28 23:03:40 +0900

    Use primitive REPL for test

--- test/yamatanooroti/test_multiplatform.rb
+++ test/yamatanooroti/test_multiplatform.rb
@@ -2,17 +2,24 @@ require 'yamatanooroti'
 
 class Yamatanooroti::TestMultiplatform < Yamatanooroti::TestCase
   def setup
-    start_terminal(5, 50, ['ruby', '-rreadline', '-e', %q[puts "=> #{Readline.readline('prompt> ')}"]])
+    repl_code = <<~'CODE'
+      loop {
+        result = Readline.readline('prompt> ')
+        puts "=> #{result}"
+      }
+    CODE
+    start_terminal(5, 30, ['ruby', '-rreadline', '-e', repl_code])
     sleep 0.5
   end
 
   def test_example
     write(":a\n")
     close
-    assert_screen(['prompt> :a', '=> :a', '', '', ''])
+    assert_screen(['prompt> :a', '=> :a', 'prompt>', '', ''])
     assert_screen(<<~EOC)
       prompt> :a
       => :a
+      prompt>
     EOC
   end
 end
