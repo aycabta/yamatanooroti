99397b0046ba38015f3344bf34803b1377848e3b
aycabta <aycabta@gmail.com>
2021-04-13 21:45:40 +0900

    Add a test for meta key input

--- test/yamatanooroti/test_run_ruby.rb
+++ test/yamatanooroti/test_run_ruby.rb
@@ -23,4 +23,41 @@ class Yamatanooroti::TestRunRuby < Yamatanooroti::TestCase
       aaa
     EOC
   end
+
+  def test_meta_key
+    get_into_tmpdir
+    start_terminal(5, 30, ['ruby', '-rreline', '-e', 'Reline.readline(%{?})'])
+    write('aaa ccc')
+    write("\M-b")
+    write('bbb ')
+    close
+    assert_screen(<<~EOC)
+      ?aaa bbb ccc
+    EOC
+  ensure
+    get_out_from_tmpdir
+  end
+
+  private
+
+  def get_into_tmpdir
+    @pwd = Dir.pwd
+    suffix = '%010d' % Random.rand(0..65535)
+    @tmpdir = File.join(File.expand_path(Dir.tmpdir), "test_yamatanooroti_#{$$}_#{suffix}")
+    begin
+      Dir.mkdir(@tmpdir)
+    rescue Errno::EEXIST
+      FileUtils.rm_rf(@tmpdir)
+      Dir.mkdir(@tmpdir)
+    end
+    @inputrc_backup = ENV['INPUTRC']
+    @inputrc_file = ENV['INPUTRC'] = File.join(@tmpdir, 'temporaty_inputrc')
+    File.unlink(@inputrc_file) if File.exist?(@inputrc_file)
+  end
+
+  def get_out_from_tmpdir
+    FileUtils.rm_rf(@tmpdir)
+    ENV['INPUTRC'] = @inputrc_backup
+    ENV.delete('RELINE_TEST_PROMPT') if ENV['RELINE_TEST_PROMPT']
+  end
 end
